5888618dccc53e79be01692f2c4808e665f4f770
Sunny Bejugam <Sunny.Bejugam@fdmgroup.com>
2024-10-05 16:04:28 +0100

    Refactor the Checkout class and CheckoutTests class to accept Pricing Rules

--- CheckoutKataApp.Core/Checkout.cs
+++ CheckoutKataApp.Core/Checkout.cs
@@ -8,8 +8,14 @@ namespace CheckoutKataApp.Core
 {
     public class Checkout
     {
+        private Dictionary<string, (int UnitPrice, int OfferQuantity, int OfferPrice)> _pricingRules;
         private Dictionary<string, int> _itemCounts = [];
         private int _totalPrice = 0;
+
+        public Checkout(Dictionary<string, (int UnitPrice, int OfferQuantity, int OfferPrice)> pricingRules)
+        {
+                _pricingRules = pricingRules;
+        }
 
         public object? GetTotalPrice()
         {
@@ -39,33 +45,37 @@ namespace CheckoutKataApp.Core
                 string itemKey = item.Key;
                 int itemCount = item.Value;
 
-                if (itemKey == "A")
-                {
-                    // Apply the special offer "3 for 130" and remaining items at 50 each
-                    int specialOfferGroups = itemCount / 3;    // How many groups of 3 "A"s can be used?
-                    int remainingItems = itemCount % 3;        // How many leftover items to charge at the normal price?
+                var (unitPrice, offerQuantity, offerPrice) = _pricingRules[itemKey];
+                _totalPrice += (itemCount / offerQuantity) * offerPrice + (itemCount % offerQuantity) * unitPrice;
 
-                    _totalPrice += specialOfferGroups * 130 + remainingItems * 50;
 
-                }
-                else if (itemKey == "B")
-                {
-                    // Apply the special offer "2 for 45" and remaining items at 30 each
-                    int specialOfferGroups = itemCount / 2;    // How many groups of 2 "B"s can be used?
-                    int remainingItems = itemCount % 2;        // How many leftover items to charge at the normal price?
+                //if (itemKey == "A")
+                //{
+                //    // Apply the special offer "3 for 130" and remaining items at 50 each
+                //    int specialOfferGroups = itemCount / 3;    // How many groups of 3 "A"s can be used?
+                //    int remainingItems = itemCount % 3;        // How many leftover items to charge at the normal price?
 
-                    _totalPrice += specialOfferGroups * 45 + remainingItems * 30;
-                }
-                else if (itemKey == "C") 
-                {
-                    // No special offer for "C", just charge 20 for each
-                    _totalPrice += itemCount * 20;
-                }
-                else if (itemKey == "D")
-                {
-                    // No special offer for "D", just charge 15 for each
-                    _totalPrice += itemCount * 15;
-                }
+                //    _totalPrice += specialOfferGroups * 130 + remainingItems * 50;
+
+                //}
+                //else if (itemKey == "B")
+                //{
+                //    // Apply the special offer "2 for 45" and remaining items at 30 each
+                //    int specialOfferGroups = itemCount / 2;    // How many groups of 2 "B"s can be used?
+                //    int remainingItems = itemCount % 2;        // How many leftover items to charge at the normal price?
+
+                //    _totalPrice += specialOfferGroups * 45 + remainingItems * 30;
+                //}
+                //else if (itemKey == "C") 
+                //{
+                //    // No special offer for "C", just charge 20 for each
+                //    _totalPrice += itemCount * 20;
+                //}
+                //else if (itemKey == "D")
+                //{
+                //    // No special offer for "D", just charge 15 for each
+                //    _totalPrice += itemCount * 15;
+                //}
             }
         }
     }
--- CheckoutKataApp.Tests/CheckoutTests.cs
+++ CheckoutKataApp.Tests/CheckoutTests.cs
@@ -9,10 +9,25 @@ namespace CheckoutKataApp.Tests
 {
     public class CheckoutTests
     {
+        private Dictionary<string, (int UnitPrice, int OfferQuantity, int OfferPrice)> _pricingRules;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Define pricing rules for each item
+            _pricingRules = new Dictionary<string, (int UnitPrice, int OfferQuantity, int OfferPrice)>
+            {
+                {"A", (50, 3, 130)},  // "A" costs 50, offer: 3 for 130
+                {"B", (30, 2, 45)},   // "B" costs 30, offer: 2 for 45
+                {"C", (20, 0, 0)},    // "C" costs 20, no special offer
+                {"D", (15, 0, 0)}     // "D" costs 15, no special offer
+            };
+        }
+
         [Test]
         public void Scan_SingleItem_ReturnsTheCorrectPrice()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(_pricingRules);
             checkout.Scan("A");
             Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
         }
@@ -20,7 +35,7 @@ namespace CheckoutKataApp.Tests
         [Test]
         public void Scan_MultipleItesmsWithOutOffer_ReturnsTheCorrectTotalPrice()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(_pricingRules);
             checkout.Scan("A");
             checkout.Scan("B");
             Assert.That(checkout.GetTotalPrice(), Is.EqualTo(80));
@@ -29,7 +44,7 @@ namespace CheckoutKataApp.Tests
         [Test]
         public void Scan_MultupleItemsWithOffer_ReturnsTheCorrectTotalPrice()
         {
-            var checkout = new Checkout();
+            var checkout = new Checkout(_pricingRules);
             checkout.Scan("A");
             checkout.Scan("A");
             checkout.Scan("A");
